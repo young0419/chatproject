5fe3ffee6c167defe917b12f826053b4deee75d4
EunYoung Seo <young419@kaoni.com>
2020-08-19 15:00:49 +0900

    server connection start

--- src/application/chat/server/ChatServer.java
+++ src/application/chat/server/ChatServer.java
@@ -1,5 +1,38 @@
 package application.chat.server;
 
-public class ChatServer {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+public class ChatServer {
+	
+	private static final int PORT = 9999;
+	private ServerSocket serverSocket;
+	static List<ServerThread> clientList = new CopyOnWriteArrayList<ServerThread>();
+	
+	public void startServer() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					serverSocket = new ServerSocket(PORT);
+					System.out.println("서버 시작");
+					
+					while (true) {
+						Socket socket = serverSocket.accept();
+						System.out.println("클라이언트 연결 [ " + socket+" ]");
+						
+						ServerThread serverThread = new ServerThread(socket);
+						clientList.add(serverThread);
+						new Thread(serverThread).start();						
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		}).start();
+	}
 }
