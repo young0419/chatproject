2bfbcf81276591eb1651595babe001510d2d770c
EunYoung Seo <young419@kaoni.com>
2020-08-19 18:39:06 +0900

    server client list class change

--- src/application/chat/server/ChatServer.java
+++ src/application/chat/server/ChatServer.java
@@ -3,36 +3,49 @@ package application.chat.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.Vector;
 
 public class ChatServer {
-	
+
 	private static final int PORT = 9999;
 	private ServerSocket serverSocket;
-	static List<ServerThread> clientList = new CopyOnWriteArrayList<ServerThread>();
-	
-	public void startServer() {
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					serverSocket = new ServerSocket(PORT);
-					System.out.println("서버 시작");
-					
-					while (true) {
-						Socket socket = serverSocket.accept();
-						System.out.println("클라이언트 연결 [ " + socket+" ]");
-						
-						ServerThread serverThread = new ServerThread(socket);
-						clientList.add(serverThread);
-						new Thread(serverThread).start();						
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+	private Vector<ServerThread> clientList = new Vector<ServerThread>();
+
+	void startServer() {
+
+		try {
+			serverSocket = new ServerSocket(PORT);
+			System.out.println("서버 시작");
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		try {
+			while (true) {
+				Socket socket = serverSocket.accept();
+				System.out.println("클라이언트 연결 [ " + socket + " ]");
+				ServerThread serverThread = new ServerThread(socket, clientList);
+				new Thread(serverThread).start();
 				
+				clientList.add(serverThread);
 			}
-		}).start();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+	}
+	
+	public void closeSocket() {
+		try {
+			if (serverSocket != null && !serverSocket.isClosed())
+				serverSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		new ChatServer().startServer();
 	}
 }
