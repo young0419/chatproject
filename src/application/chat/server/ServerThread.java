01db51f4a2c4b35083fc7e376aab1d9f9188c9a4
EunYoung Seo <young419@kaoni.com>
2020-08-19 18:39:44 +0900

    serverthread run method change

--- src/application/chat/server/ServerThread.java
+++ src/application/chat/server/ServerThread.java
@@ -6,47 +6,77 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Vector;
 
 public class ServerThread implements Runnable {
 
+	private Vector<ServerThread> clientList;
 	private Socket socket;
 	private String nickName;
 	private BufferedWriter bw;
 	private BufferedReader br;
 
-	public ServerThread(Socket socket) {
+	public ServerThread(Socket socket, Vector<ServerThread> clientList) {
 		this.socket = socket;
+		this.clientList = clientList;
 	}
 
 	@Override
-	public void run() {
+	public synchronized void run() {
 		try {
 			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-			String message = null;
-			while(true) {
-				if (null == nickName) {
+
+			while (true) {
+				System.out.println("메시지받기전");
+				if(null == nickName) {
 					nickName = br.readLine();
-					message = nickName + "님이 접속하셨습니다.";
-					sendMessageToAll(message);
+					String message = nickName + "님이 접속하셨습니다.";
+					System.out.println(message);
+					sendGreeting(message);
 				} else {
-					message = br.readLine();
+					String message = br.readLine();
+					System.out.println("message >> " + message);
+					sendMessageToAll(message);
 				}
-				sendMessageToAll(message);				
 			}
 		} catch (IOException e) {
-			e.printStackTrace();
+			clientList.remove(this);
+			String message = "[클라이언트 통신 안됨: " + socket.getRemoteSocketAddress() + ": "
+					+ Thread.currentThread().getName() + " ]";
+			System.out.println(message);
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+			}
+			try {
+				bw.close();
+			} catch (IOException e) {
+			}
+			try {
+				socket.close();
+			} catch (IOException e) {
+			}
+		}
+	}
+
+	private void sendGreeting(String message) throws IOException {
+		for (ServerThread st : clientList) {
+			if (!st.socket.equals(this.socket)) {
+				st.sendMessage(message);
+			}
 		}
 	}
 
 	private void sendMessageToAll(String message) throws IOException {
-		for (ServerThread st : ChatServer.clientList) {
+		for (ServerThread st : clientList) {
 			st.sendMessage(message);
 		}
 	}
 
 	private void sendMessage(String message) throws IOException {
-		bw.write(message);
+		bw.write(message + "\n");
 		bw.flush();
 	}
 
